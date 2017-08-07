eda14c218b345ceb51748e8125a06242a0d27671
nickross48 <nickross48@gmail.com>
2017-08-07 15:11:14 -0400

    Tab bar VC started

--- Tracks/View Controllers/ProfileViewController.swift
+++ Tracks/View Controllers/ProfileViewController.swift
@@ -1,25 +1,24 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  Tracks
 //
-//  Created by Nicholas Ross on 2017-08-02.
+//  Created by Nicholas Ross on 2017-08-07.
 //  Copyright © 2017 Nicholas Ross. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ProfileViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
---
+++ Tracks/View Controllers/RecordViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RecordViewController.swift
+//  Tracks
+//
+//  Created by Nicholas Ross on 2017-08-07.
+//  Copyright © 2017 Nicholas Ross. All rights reserved.
+//
+
+import UIKit
+
+
+class RecordViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
