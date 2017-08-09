fe314eee667f892c66e607e05d07f3e61cdc03ab
nickross48 <nickross48@gmail.com>
2017-08-09 10:46:38 -0400

    Refactored storyboards in tab bar controller

---
+++ Tracks/View Controllers/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  Tracks
+//
+//  Created by Nicholas Ross on 2017-08-09.
+//  Copyright © 2017 Nicholas Ross. All rights reserved.
+//
+
+import UIKit
+
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
