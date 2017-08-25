8010842d73c15be84fe85848b41c92e34c21c108
nickross48 <nickross48@gmail.com>
2017-08-25 17:06:54 -0400

    User persistancy

--- Tracks/AppDelegate.swift
+++ Tracks/AppDelegate.swift
@@ -23,17 +23,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         IQKeyboardManager.sharedManager().enable = true
 
+        let defaults = UserDefaults.standard
+        let initialViewController: UIViewController
         
-        // create an instance of login storyboard that has loginVC set as initial VC
-        let storyboard = UIStoryboard(name: "Login", bundle: .main)
-        
-        // check if storyboard has an initial VC set
-        if let initialViewController = storyboard.instantiateInitialViewController() {
-            // if storyboard's initial VC exists set it to the window's rootVC property
-            window?.rootViewController = initialViewController
-            // position window above other existing windows
-            window?.makeKeyAndVisible()
+        if Auth.auth().currentUser != nil,
+            let userData = defaults.object(forKey: Constants.UserDefaults.currentUser) as? Data,
+            let user = NSKeyedUnarchiver.unarchiveObject(with: userData) as? User {
+            
+            User.setCurrent(user)
+            
+            initialViewController = UIStoryboard.initialViewController(for: .main)
+        }  else {
+            initialViewController = UIStoryboard.initialViewController(for: .login)
         }
+        
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+
+//        // create an instance of login storyboard that has loginVC set as initial VC
+//        let storyboard = UIStoryboard(name: "Login", bundle: .main)
+//        
+//        // check if storyboard has an initial VC set
+//        if let initialViewController = storyboard.instantiateInitialViewController() {
+//            // if storyboard's initial VC exists set it to the window's rootVC property
+//            window?.rootViewController = initialViewController
+//            // position window above other existing windows
+//            window?.makeKeyAndVisible()
+//        }
         
         return true
     }
@@ -64,58 +80,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
+
+
+
 }
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        FirebaseApp.configure()
-        return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
-extension AppDelegate {
-    func configureInitialRootViewController(for window: UIWindow?) {
-        let defaults = UserDefaults.standard
-        let initialViewController: UIViewController
-        
-        if Auth.auth().currentUser != nil,
-            let userData = defaults.object(forKey: Constants.UserDefaults.currentUser) as? Data,
-            let user = NSKeyedUnarchiver.unarchiveObject(with: userData) as? User {
-            
-            User.setCurrent(user)
-            
-            initialViewController = UIStoryboard.initialViewController(for: .main)
-        }  else {
-            initialViewController = UIStoryboard.initialViewController(for: .login)
-        }
-        
-        window?.rootViewController = initialViewController
-        window?.makeKeyAndVisible()
-    }
-}
-
 
 // <a href="https://icons8.com/icon/24717/Add">Add icon credits</a> l
 // link for add icon
@@ -132,3 +100,6 @@ extension AppDelegate {
 
 //<a href="https://icons8.com/icon/14096/Home">Home icon credits</a>
 // link for home icon
+
+// <a href="https://icons8.com/icon/26217/Logout-Rounded-Left-Filled">Logout rounded left filled icon credits</a>
+// link for logout button
